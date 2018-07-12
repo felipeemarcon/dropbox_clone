3c7b981d9110051d3ba450088508234439204f24
Felipe Marcon <felipeemarcon@gmail.com>
2018-07-11 22:29:04 -0300

    Select files (css)

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -356,6 +356,8 @@ class DropBoxController {
       <div class="name text-center">${file.name}</div>
     `
 
+    this.initEventsLi(li);
+
     return li;
 
   }
@@ -376,6 +378,16 @@ class DropBoxController {
         this.listFilesEl.appendChild(this.getFileView(data, key));
 
       });
+
+    });
+
+  }
+
+  initEventsLi(li){
+
+    li.addEventListener('click', e => {
+
+      li.classList.toggle('selected');
 
     });
 
